f559d61e0004ccf1926465338768bbfec1f3b399
smvax <sharov8055@gmail.com>
2026-09-23 20:35:42 +0300

    Сделана приложуха под код со степика

--- App/main.cpp
+++ App/main.cpp
@@ -1,7 +1,21 @@
 ﻿#include "sea.h"
+#include "game.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+    std::setlocale(LC_ALL, "rus");
+
+    try {
+        Game sea_battle;
+        sea_battle.start();
+    }
+    catch (const std::exception& ex) {
+        std::cerr << std::endl << "CRITICAL ERROR: " << ex.what() << std::endl;
+        return 1;
+    }
+    catch (...) {
+        std::cerr << std::endl << "UNEXPECTED CRITICAL ERROR!!!" << std::endl;
+        return 1;
+    }
+
     return 0;
 }
--- SeaLib/sea.h
+++ SeaLib/sea.h
@@ -3,6 +3,7 @@
 #include <stdexcept>
 #include <string>
 #include <ctime>
+#include <clocale>
 
 enum Direction { Horizontal, Vertical };
 
